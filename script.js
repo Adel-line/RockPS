180f8e2c75e7acd27c030b453a04a89827e53031
Adelina <adelina.sahlean@gmail.com>
2021-02-04 20:45:33 +0100

    adding the files

---
+++ script.js
@@ -0,0 +1,21 @@
+init();
+
+function init() {
+  console.log("initialize");
+}
+
+function userchoice() {
+    console.log("userchoice");
+};
+
+}
+
+function pcschoice() {}
+
+function showtimer() {}
+
+function showchoices() {}
+
+function winner() {}
+
+function restart() {}
